e5b51f60572ad19905c6367f28bf1667836f014d
SHT <tasos@sht.gr>
2019-02-27 21:38:17 +0200

    Initial Commit

--- scanner.py
+++ scanner.py
@@ -1,88 +1,190 @@
 """
 Sample script to test ad-hoc scanning by table drive.
 This accepts a number with optional decimal part [0-9]+(\.[0-9]+)?
-
 NOTE: suitable for optional matches
 """
 
-def getchar(text,pos):
-	""" returns char category at position `pos` of `text`,
-	or None if out of bounds """
-	
-	if pos<0 or pos>=len(text): return None
-	
-	c = text[pos]
-	
-	# **Σημείο #3**: Προαιρετικά, προσθέστε τις δικές σας ομαδοποιήσεις
-	
-	if c>='0' and c<='9': return 'DIGIT'	# 0..9 grouped together
-	
-	if c=='.': return 'DOT'	# dot as a category by itself
-	
-	return c	# anything else
-	
+
+def getchar(text, pos):
+    """ returns char category at position `pos` of `text`,
+    or None if out of bounds """
+
+    if pos < 0 or pos >= len(text):
+        return None
+
+    c = text[pos]
+
+    # **Σημείο #3**: Προαιρετικά, προσθέστε τις δικές σας ομαδοποιήσεις
+
+    return c  # anything else
 
 
-def scan(text,transitions,accepts):
-	""" scans `text` while transitions exist in
-	'transitions'. After that, if in a state belonging to
-	`accepts`, it returns the corresponding token, else ERROR_TOKEN.
-	"""
-	
-	# initial state
-	pos = 0
-	state = 's0'
-	# memory for last seen accepting states
-	last_token = None
-	last_pos = None
-	
-	
-	while True:
-		
-		c = getchar(text,pos)	# get next char (category)
-		
-		if state in transitions and c in transitions[state]:
-			state = transitions[state][c]	# set new state
-			pos += 1	# advance to next char
-			
-			# remember if current state is accepting
-			if state in accepts:
-				last_token = accepts[state]
-				last_pos = pos
-			
-		else:	# no transition found
+def scan(text, transitions, accepts):
+    """ scans `text` while transitions exist in
+    'transitions'. After that, if in a state belonging to
+    `accepts`, it returns the corresponding token, else ERROR_TOKEN.
+    """
 
-			if last_token is not None:	# if an accepting state already met
-				return last_token,last_pos
-			
-			# else, no accepting state met yet
-			return 'ERROR_TOKEN',pos
-			
-	
+    # initial state
+    pos = 0
+    state = 'q0'
+    # memory for last seen accepting states
+    last_token = None
+    last_pos = None
+
+    while True:
+
+        c = getchar(text, pos)  # get next char (category)
+
+        if state in transitions and c in transitions[state]:
+            state = transitions[state][c]  # set new state
+            pos += 1  # advance to next char
+
+            # remember if current state is accepting
+            if state in accepts:
+                last_token = accepts[state]
+                last_pos = pos
+
+        else:  # no transition found
+
+            if last_token is not None:  # if an accepting state already met
+                return last_token, last_pos
+
+            # else, no accepting state met yet
+            return 'ERROR_TOKEN', pos
+
+
 # **Σημείο #1**: Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων
-transitions = { 's0': { 'DIGIT':'s1' },
-       			's1': { 'DIGIT':'s1','DOT':'s2' },
-       			's2': { 'DIGIT':'s3' },
-       			's3': { 'DIGIT':'s3' }       
-     		  } 
+transitions = {
+    'q0': {
+        '0': 'q1',
+        '1': 'q1',
+        '2': 'q1',
+        '3': 'q2'
+    },
+    'q1': {
+        '0': 'q3',
+        '1': 'q3',
+        '2': 'q3',
+        '3': 'q3',
+        '4': 'q3',
+        '5': 'q3',
+        '6': 'q3',
+        '7': 'q3',
+        '8': 'q3',
+        '9': 'q3'
+    },
+    'q2': {
+        '0': 'q3',
+        '2': 'q3',
+        '2': 'q3',
+        '3': 'q3',
+        '4': 'q3',
+        '5': 'q3'
+    },
+    'q3': {
+        '0': 'q4'
+    },
+    'q4': {
+        '0': 'q5',
+        '1': 'q5',
+        '2': 'q5',
+        '3': 'q5',
+        '4': 'q5',
+        '5': 'q5',
+        '6': 'q5',
+        '7': 'q5',
+        '8': 'q5',
+        '9': 'q5'
+    },
+    'q5': {
+        '0': 'q6',
+        '1': 'q6',
+        '2': 'q6',
+        '3': 'q6',
+        '4': 'q6',
+        '5': 'q6',
+        '6': 'q6',
+        '7': 'q6',
+        '8': 'q6',
+        '9': 'q6'
+    },
+    'q6': {
+        'G': 'q7',
+        'K': 'q10',
+        'M': 'q12'
+    },
+    'q7': {
+        '0': 'q8',
+        '1': 'q8',
+        '2': 'q8',
+        '3': 'q8',
+        '4': 'q8',
+        '5': 'q8',
+        '6': 'q8',
+        '7': 'q8',
+        '8': 'q8',
+        '9': 'q8'
+    },
+    'q8': {
+        '0': 'q9',
+        '1': 'q9',
+        '2': 'q9',
+        '3': 'q9',
+        '4': 'q9',
+        '5': 'q9',
+        '6': 'q9',
+        '7': 'q9',
+        '8': 'q9',
+        '9': 'q9'
+    },
+    'q9': {
+        'K': 'q10',
+        'M': 'q12'
+    },
+    'q10': {
+        'T': 'q11'
+    },
+    'q12': {
+        'P': 'q13'
+    },
+    'q13': {
+        'S': 'q14'
+    }
+}
 
 # **Σημείο #2**: Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής
-accepts = { 's1':'INT_TOKEN',
-       		's3':'FLOAT_TOKEN'	
-     	  }
+accepts = {
+    'q11': 'WIND_TOKEN',
+    'q14': 'WIND_TOKEN'
+}
 
+# inputs = [
+#     "36004KT", "36007KT", "36007KT", "36008KT",
+#     "36008KT", "06004KT", "15004KT", "10004KT",
+#     "10005KT", "05005KT", "04005KT", "07005KT",
+#     "04008KT", "03009KT", "03012KT", "02010KT",
+#     "01010KT", "01011KT", "03012KT", "03013KT",
+#     "01012KT", "01011KT", "36010KT", "36007KT",
+#     "28002KT", "33005KT", "36009KT", "04002KT",
+#     "24004KT", "24004KT", "23004KT", "00000KT",
+#     "24002KT", "25002KT", "29002KT", "24002KT",
+#     "00000KT", "30005KT", "30003KT", "VRB02KT",
+#     "31008KT", "31010KT", "31011KT", "29009KT",
+#     "33012KT", "29012KT", "31011KT", "31016KT",
+#     "32017KT", "33017KT"
+# ]
 
 # get a string from input
 text = input('give some input>')
 
 # scan text until no more input
-while text:		# i.e. len(text)>0
-	# get next token and position after last char recognized
-	token,pos = scan(text,transitions,accepts)
-	if token=='ERROR_TOKEN':
-		print('unrecognized input at position',pos,'of',text)
-		break
-	print("token:",token,"text:",text[:pos])
-	# new text for next scan
-	text = text[pos:]
-	
+while text:        # i.e. len(text)>0
+    # get next token and position after last char recognized
+    token, pos = scan(text, transitions, accepts)
+    if token == 'ERROR_TOKEN':
+        print('unrecognized input at position', pos, 'of', text)
+        break
+    print("token:", token, "text:", text[:pos])
+    # new text for next scan
+    text = text[pos:]
